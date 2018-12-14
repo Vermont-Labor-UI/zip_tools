64b8121169fba62358d0fdec54269cb134f32bd8
Chris Woerz <chriswoerz@gmail.com>
2018-12-14 11:19:30 -0700

    testing using globbing

--- Add-ToZIPFile.ps1
+++ Add-ToZIPFile.ps1
@@ -1,17 +1,43 @@
 [CmdletBinding()]
 Param(
-    [string]$ZIPFileName,
-    [string]$NewFileToAdd
+    [string]$zipFileNamePattern,
+    [string]$newFileToAddPattern
 )
 
-try {
-    [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-    $zip = [System.IO.Compression.ZipFile]::Open($ZIPFileName,"Update")
-    $FileName = [System.IO.Path]::GetFileName($NewFileToAdd)
-    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip,$NewFileToAdd,$FileName,"Optimal") | Out-Null
-    $Zip.Dispose()
-    Write-Host "Successfully added $NewFileToAdd to $ZIPFileName "
-} catch {
-    Write-Warning "Failed to add $NewFileToAdd to $ZIPFileName . Details : $_"
+function ResolveFilePattern {
+    param (
+        [string]$fileNamePattern
+    )
 
+    return $fileNamePattern | Resolve-Path
+}
+
+function AddFileToZipAtPath {
+    param (
+        [string]$zipFileName,
+        [string]$newFileToAdd,
+        [string]$pathInZip
+    )
+
+    try {
+        [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+        $zip = [System.IO.Compression.ZipFile]::Open($zipFileName, "Update")
+        $fileName = [System.IO.Path]::GetFileName($newFileToAdd)
+        [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $newFileToAdd, $fileName, "Optimal") | Out-Null
+        $Zip.Dispose()
+        Write-Host "Successfully added $newFileToAdd to $zipFileName "
+    }
+    catch {
+        Write-Warning "Failed to add $newFileToAdd to $zipFileName . Details : $_"
+
+    }
+    
+}
+
+ResolveFilePattern($zipFileNamePattern) | ForEach-Object {
+    $zipFileName = $_
+    ResolveFilePattern($newFileToAddPattern) | ForEach-Object {
+        $newFileName = $_
+        Write-Host "Found $zipFileName and $newFileName"
+    }
 }
